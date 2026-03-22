7536a0c41755431e192f98179295423ad4e531da
Alison Baldwin <alibaldwin@uiowa.edu>
2026-03-21 20:15:35 -0500

    added api links

---
+++ data/apis.js
@@ -0,0 +1,14 @@
+    //real time, immediate results for dissolved oxygen in iowa
+// https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=IA&parameterCd=00300&period=P1D
+
+    //turbidity
+// https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=IA&parameterCd=63680&period=P1D
+
+    //conductance
+// https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=IA&parameterCd=00095&period=P1D
+
+    //ph
+// https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=IA&parameterCd=00400&period=P1D
+
+    // all combined:
+// https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=IA&parameterCd=00300,63680,00095,00400&period=P1D
